2e062a5f5a1b742344fed9336ac670fbb75aad33
kyleconkright <kyleconkright@gmail.com>
2017-06-17 00:23:14 -0700

    server.js fix

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express   = require('express');
+const bodyParser = require("body-parser");
 const router    = require('./router/');
 const appRouter = express.Router();
 
@@ -8,9 +9,15 @@ const PREFIX    = '/hipstr-api/';
 const PORT      = process.env.PORT || 4201;
 
 const app = express();
+app.use(bodyParser.json());
 
 router.route(appRouter);
 app.use(PREFIX, appRouter);
+
+// Create link to Angular build directory
+var distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
+
 
 app.listen(PORT, () => {
   console.log(`LISTENING TO ${PREFIX}  ON: ${PORT}! While it's still cool... `);
