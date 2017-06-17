b942b3899066727e57bd4931d6780e35511d9970
kyleconkright <kyleconkright@gmail.com>
2017-06-17 01:08:54 -0700

    server.js fix

--- server/server.js
+++ server/server.js
@@ -1,24 +1,38 @@
-'use strict';
+// 'use strict';
 
-const express   = require('express');
-const bodyParser = require("body-parser");
-const path = require('path');
-const router    = require('./router/');
+// const express   = require('express');
+// const bodyParser = require("body-parser");
+// const path = require('path');
+// const router    = require('./router/');
+// const appRouter = express.Router();
+
+// const PREFIX    = '/hipstr-api/';
+// const PORT      = process.env.PORT || 8080;
+
+// const app = express();
+// app.use(bodyParser.json());
+
+// router.route(appRouter);
+// app.use(PREFIX, appRouter);
+
+// // Create link to Angular build directory
+// var distDir = __dirname + "/dist/";
+// app.use(express.static(distDir));
+
+// app.listen(PORT, () => {
+//   console.log(`LISTENING TO ${PREFIX}  ON: ${PORT}! While it's still cool... `);
+// });
+
+// server.js
+const express = require('express');
+const bodyParser = require('body-parser');
+const router = require('./router/');
 const appRouter = express.Router();
 
-const PREFIX    = '/hipstr-api/';
-const PORT      = process.env.PORT || 8080;
-
 const app = express();
-app.use(bodyParser.json());
-
-router.route(appRouter);
-app.use(PREFIX, appRouter);
-
-// Create link to Angular build directory
-var distDir = __dirname + "/dist/";
-app.use(express.static(distDir));
-
-app.listen(PORT, () => {
-  console.log(`LISTENING TO ${PREFIX}  ON: ${PORT}! While it's still cool... `);
-});
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
