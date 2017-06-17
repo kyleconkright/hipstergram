297e18430db5b53ed2e98ac3889e4e40b0921f7d
kyleconkright <kyleconkright@gmail.com>
2017-06-17 00:40:44 -0700

    server.js fix

--- server/server.js
+++ server/server.js
@@ -18,14 +18,6 @@ app.use(PREFIX, appRouter);
 // Create link to Angular build directory
 app.use(express.static(__dirname + '/dist'));
 
-
 app.listen(PORT, () => {
   console.log(`LISTENING TO ${PREFIX}  ON: ${PORT}! While it's still cool... `);
-});
-
-
-// For all GET requests, send back index.html
-// so that PathLocationStrategy can be used
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
