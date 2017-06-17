cbcc820c386c04248f5c68f9252624d55efd6edc
kyleconkright <kyleconkright@gmail.com>
2017-06-17 00:58:15 -0700

    server.js fix

--- server.js
+++ server.js
@@ -11,10 +11,11 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/dist'));
 
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
 
 app.listen(PORT, () => {
     console.log(`LISTENING TO ${PREFIX}  ON: ${PORT}! While it's still cool... `);
+});
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
