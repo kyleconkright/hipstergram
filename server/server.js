dfb09eccca8455333729aff69f69ed0649c26d71
kyleconkright <kyleconkright@gmail.com>
2017-06-17 01:23:40 -0700

    server.js fix

--- server/server.js
+++ server/server.js
@@ -1,25 +1,21 @@
-// 'use strict';
+// server.js
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const router = require('./router/');
+const appRouter = express.Router();
 
-// const express   = require('express');
-// const bodyParser = require("body-parser");
-// const path = require('path');
-// const router    = require('./router/');
-// const appRouter = express.Router();
+const PREFIX = '/hipstr-api/';
+const PORT = process.env.PORT || 8080;
 
-// const PREFIX    = '/hipstr-api/';
-// const PORT      = process.env.PORT || 8080;
+const app = express();
+app.use(bodyParser.json());
 
-// const app = express();
-// app.use(bodyParser.json());
+router.route(appRouter);
+app.use(PREFIX, appRouter);
 
-// router.route(appRouter);
-// app.use(PREFIX, appRouter);
+app.use(express.static(__dirname + '/dist'));
 
-// // Create link to Angular build directory
-// var distDir = __dirname + "/dist/";
-// app.use(express.static(distDir));
-
-// app.listen(PORT, () => {
-//   console.log(`LISTENING TO ${PREFIX}  ON: ${PORT}! While it's still cool... `);
-// });
-
+app.listen(PORT, () => {
+  console.log(`LISTENING TO ${PREFIX}  ON: ${PORT}! While it's still cool... `);
+});
