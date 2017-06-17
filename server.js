22bee0b6413d554145c63aa59402607112a16d06
kyleconkright <kyleconkright@gmail.com>
2017-06-17 01:20:59 -0700

    server.js fix

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 // server.js
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 const router = require('./server/router/');
 const appRouter = express.Router();
 
+const PREFIX = '/hipstr-api/';
+const PORT = process.env.PORT || 8080;
+
 const app = express();
-// Run the app by serving the static files
-// in the dist directory
+app.use(bodyParser.json());
+
+router.route(appRouter);
+app.use(PREFIX, appRouter);
+
 app.use(express.static(__dirname + '/dist'));
-// Start the app by listening on the default
-// Heroku port
-app.listen(process.env.PORT || 8080);
+
+app.listen(PORT, () => {
+    console.log(`LISTENING TO ${PREFIX}  ON: ${PORT}! While it's still cool... `);
+});
