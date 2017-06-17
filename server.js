e15630bf96d1e1c3b69ab18d113dc6a4bbee109c
kyleconkright <kyleconkright@gmail.com>
2017-06-17 01:00:02 -0700

    server.js fix

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const router = require('./router/');
 const appRouter = express.Router();
 
-const PREFIX = '/hipstr-api/';
+// const PREFIX = '/hipstr-api/';
 const PORT = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/dist'));
@@ -16,6 +16,6 @@ app.listen(PORT, () => {
     console.log(`LISTENING TO ${PREFIX}  ON: ${PORT}! While it's still cool... `);
 });
 
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
+// app.get('/*', function (req, res) {
+//     res.sendFile(path.join(__dirname + '/dist/index.html'));
+// });
