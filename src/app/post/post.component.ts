241a6fe2dc24fcd1388d65c3f626b9b85c1ef08e
kyleconkright <kyleconkright@gmail.com>
2017-06-18 11:27:38 -0700

    post styling

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-post',
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class PostComponent implements OnInit {
 
   private testPosts: Array<any> = [1, .10];
+  @Input() post: String
 
   constructor() { }
 
