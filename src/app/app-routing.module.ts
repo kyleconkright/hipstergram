671ed16224338bbd7390202b6ba0b227c1999e0b
kyleconkright <kyleconkright@gmail.com>
2017-06-24 10:33:39 -0700

    modal first pass working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,23 @@ import { Routes } from "@angular/router/router";
 import { AppComponent } from "./app.component";
 import { PostsComponent } from "./posts/posts.component";
 import { PostComponent } from "./posts/post/post.component";
+import { PostPreviewComponent } from "./posts/post-preview/post-preview.component";
 
 export const appRoutes: Routes = [
+    {
+        path: '',
+        component: PostsComponent,
+        // pathMatch: 'full'
+    },
+    {
+        path: ':id',
+        component: PostPreviewComponent,
+        outlet: 'post'
+    },
     {
         path: 'post/:id',
         component: PostComponent
     },
-    {
-        path: '',
-        component: PostsComponent
-    }
+    { path: '**', redirectTo: '' }
+
 ]
---
+++ src/app/posts/post/post.ts
@@ -0,0 +1,4 @@
+export class Post {
+    id: string;
+    name: string;
+}
